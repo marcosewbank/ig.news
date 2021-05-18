8bf74ee683ef59b409d910fbaed567baf049b432
marcosewbank <ewbankmarcos@gmail.com>
2021-05-18 14:10:50 -0300

    button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,31 @@
 import Head from "next/head";
+import React from "react";
+import { SubscribeButton } from "../components/SubscribeButton/index";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>Hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>🖐 Hey, welcome </span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/avatar.svg" alt="Girl codign" />
+      </main>
     </>
   );
 }
