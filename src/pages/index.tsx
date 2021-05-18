6a05d81ab984568a96570504179cc6a2ffcd7b53
marcosewbank <ewbankmarcos@gmail.com>
2021-05-18 10:07:43 -0300

    font sizes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import styles from "../styles/home.module.scss";
 import Head from "next/head";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inícip | ig.news</title>
+        <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello world</h1>
+
+      <h1>Hello world</h1>
     </>
   );
 }
