8752548b692056e0898381220336ff2d9eb219f0
marcos ewbank <marcos.suarez@pivotree.com>
2021-08-26 09:49:13 -0300

    tests on header and active link component

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,12 +1,32 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { ActiveLink } from ".";
 
-test("Active Link renders correctly", () => {
-  const { debug } = render(
-    <ActiveLink href="/" activeClassName="active">
-      <a>home</a>
-    </ActiveLink>
-  );
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return { asPath: "/" };
+    },
+  };
+});
 
-  debug();
+describe("ActiveLink Component", () => {
+  it("Renders correctly", () => {
+    render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>home</a>
+      </ActiveLink>
+    );
+
+    expect(screen.getByText("home")).toBeInTheDocument();
+  });
+
+  it("Is receiving active class if the link as currently active", () => {
+    render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>home</a>
+      </ActiveLink>
+    );
+
+    expect(screen.getByText("home")).toHaveClass("active");
+  });
 });
